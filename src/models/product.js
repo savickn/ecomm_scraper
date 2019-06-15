1949aa82a1bd0d714a5c86004e2b3976d8729614
Nick Savickas <nick.savickas@gmail.com>
2019-06-14 21:14:38 -0400

    br product page working, br sale page working

--- src/models/product.js
+++ src/models/product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
+  url: String, 
   name: String,
   originalPrice: String,
   salePrice: String,
--- src/scrape.js
+++ src/scrape.js
@@ -3,12 +3,100 @@ const puppeteer = require('puppeteer');
 const path = require('path');
 const util = require('util');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function autoScroll(document, window) {
+  await new Promise((resolve, reject) => {
+    var totalHeight = 0;
+    var distance = 100;
+    var timer = setInterval(() => {
+      var scrollHeight = document.body.scrollHeight;
+      window.scrollBy(0, distance);
+      totalHeight += distance;
+
+      if(totalHeight >= scrollHeight){
+        clearInterval(timer);
+        resolve();
+      }
+    }, 400);
+  });
+};
+
+const scrapeAll = async () => { }
+
+const scrapeBR = async () => { }
+const scrapeGAP = async () => { }
+const scrapeON = async () => { }
+const scrapeJC = async () => { }
+
 const scrapeHomePage = async () => {
   
 }
 
 const scrapeSaleList = async () => {
+  const salePageString = 'https://bananarepublic.gapcanada.ca/browse/category.do?cid=1014757';
+  
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  page.on('console', msg => console.log(msg.text()));
+  await page.goto(salePageString, {waitUntil: 'networkidle2'});
+  //await sleep(2000);
 
+  const result = await page.evaluate(async () => {
+    let anchors = []; // collect product links
+    var hasNextPage; // track pagination
+
+    do {
+      // pagination logic
+      const nextPage = document.querySelector("a.basic-pagination--button[title='next']");
+      console.log('nextpage --> ', nextPage.className);
+      for(let c of nextPage.classList) {
+        console.log('class --> ', c);
+      }
+      hasNextPage = !nextPage.classList.contains('pagination-inactive');
+      console.log('hasNextPage --> ', hasNextPage);
+
+      // autoScrollPage logic
+      await new Promise((resolve, reject) => {
+        var totalHeight = 0;
+        var distance = 100;
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+    
+          if(totalHeight >= scrollHeight){
+            clearInterval(timer);
+            resolve();
+          }
+        }, 20);
+      });
+
+      // scraping logic
+      const products = [...document.querySelectorAll('div.product-card')];
+      console.log('# of products --> ', products.length);
+      for(let p of products) {
+        const anchor = p.querySelector('a');
+        if(anchor) {
+          console.log('product link --> ', anchor.href);
+          anchors.push(anchor.href);
+        }
+      }
+
+      //move to next page
+      if(hasNextPage) {
+        nextPage.click();
+      }
+    } while(hasNextPage);
+
+    return anchors;
+  });
+  //console.log('result --> ', result);
+  console.log('arr length --> ', result.length);
+
+  await browser.close();
 }
 
 const scrapeProductPage = async () => {
@@ -30,7 +118,7 @@ const scrapeProductPage = async () => {
   console.log('title --> ', t.toString());*/
 
   console.log('evaluating');
-  const result = await page.evaluate(async () => {
+  const result = await page.evaluate(() => {
     console.log('evaluated');
     const title = document.querySelectorAll('h1.product-title')[0].textContent.trim();
     const price = document.querySelectorAll('h5.product-price')[0].textContent.trim();
@@ -45,7 +133,7 @@ const scrapeProductPage = async () => {
 
     // represents each section of colors (e.g. where each has its own specific price)
     const colorRadioContainers = [...document.querySelectorAll('div.swatch-price-group')];
-    //console.log('\n radio containers --> ', JSON.stringify(colorRadioContainers));
+    //console.log('radio containers --> ', JSON.stringify(colorRadioContainers));
     
     /*for(let c of colorRadioContainers) {
       console.log('container --> ', c);
@@ -59,17 +147,18 @@ const scrapeProductPage = async () => {
       console.log('container properties --> ', cProps);
     }; */
     
-    colorRadioContainers.forEach((container) => {
+    for(let container of colorRadioContainers) {
       // represents price of that specific color
       const currentPrice = isDiscounted ? container.querySelector('div.product-price__highlight').textContent.trim() : price;
       console.log('\n currentPrice --> ', currentPrice);
 
       // represents each 'color' radio button in the current container
       const colorRadios = [...container.querySelectorAll('input.swatch__radio')];
-      //console.log('\n color radios --> ', JSON.stringify(colorRadios));
-
+      //console.log('color radios --> ', JSON.stringify(colorRadios));
+      
       // represents colors from the current container
-      let interColors = colorRadios.map( async (cr) => {
+      let interColors = [];
+      for(let cr of colorRadios) {
         const colorName = cr.getAttribute('value');
         cr.click(); // selects color
 
@@ -77,77 +166,73 @@ const scrapeProductPage = async () => {
 
         // add check for unavailable sizes
         const waistSection = document.getElementById('waist');
-        console.log('waistSection --> ', waistSection);
-        console.log('waistSection css --> ', waistSection.style);
-        
+        //console.log('waistSection --> ', waistSection);
+
         /*console.log('waistSection attrs --> ', JSON.stringify(waistSection.attributes));
         const wsJson = await waistSection.jsonValue();
         console.log('waistSection json --> ', wsJson);
         const wsProperties = await waistSection.getProperties();
         console.log('waistSection properties --> ', wsProperties);*/
 
-
         const waistRadios = [...waistSection.querySelectorAll('input.swatches--radio')];
-        console.log('waistRadios --> ', waistRadios);
+        //console.log('waistRadios --> ', waistRadios);
 
-        waistRadios.forEach((wr) => {
+        for(let wr of waistRadios) {
           const waistSize = wr.getAttribute('value');
-          console.log(waistSize);
           wr.click();
 
           const lengthSection = document.getElementById('length');
           const lengthRadios = [...lengthSection.querySelectorAll('input.swatches--radio')]
-          lengthRadios.forEach((lr) => {
-            // check for 'svg.outofstock'
-            console.log('lengthRadio --> ', lr);
-            const siblingSpan = lr.nextSibling;
-            const cn = siblingSpan.getAttribute('className');
-            const cl = siblingSpan.getAttribute('classList');
-            console.log('siblingSpan --> ', siblingSpan);
-            console.log('sibling classname --> ', cn);
-            console.log('sibling classlist --> ', cl);
+          for(let lr of lengthRadios) {
+            //console.log('lengthRadio --> ', lr);
+            const parent = lr.parentElement;
+            //console.log('parent --> ', parent);
             
-            if(cl.contains('swatches--unavailable')) {
-              return;
-            }
+            // check for 'svg.outofstock'
+            const oos = parent.querySelector('svg.swatch--outOfStockIndicator');
+            //console.log('oos svg --> ', oos);
+            
+            if(oos) {
+              continue;
+            };
 
             const lengthSize = lr.getAttribute('value');
             const size = waistSize + ' x ' + lengthSize;
             console.log('size --> ', size);
             sizes.push(size);
-          });
-          //console.log('lengthRadios --> ', lengthRadios);
-        });
+          };
+        };
   
-        return {
+        interColors.push({
           colorName,
           colorPrice: currentPrice, 
-          sizes, 
-        };
-      });
+          sizes,
+        });
+      };
 
       // merges colors from current container with the page-wide color collection
       colors = [...colors, ...interColors];
-    });
+    };
 
     return {
       title,
       price,
       colors,  
     }; 
-  }, util)
+  })
   console.log('result --> ', result);
 
-  const c = result.colors[4];
+  /*const c = result.colors[4];
   console.log('\n \n ', c.colorName);
   c.sizes.forEach((size) => {
     console.log(size);
-  });
+  });*/
 
   await browser.close();
 }
 
-scrapeProductPage();
+scrapeSaleList();
+//scrapeProductPage();
 
 module.exports = {
   scrapeProductPage,
@@ -156,7 +241,7 @@ module.exports = {
 }
 
 
-
+/* Product Page Logic */
 
 //await page.screenshot({path: path.resolve('output', 'example.png')});
 //await page.pdf({path: path.resolve('output', 'hn.pdf'), format: 'A4'});
@@ -191,3 +276,12 @@ const imageNames = images.map((img) => {
   const sizes = sizeRadios.map((sr) => {
     return sr.getAttribute('value');  //sr.textContent.trim();
   });*/
+
+
+  /* Sale List Logic */
+
+  /*const p = await page.$('div.product-card');
+  console.log('product --> ', p);
+  console.log('product json --> ', await p.jsonValue());
+  console.log('product string --> ', p.toString());
+  console.log('product props --> ', await p.getProperties());*/
